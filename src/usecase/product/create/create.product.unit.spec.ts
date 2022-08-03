86598df9a14c3f88b1b9cbca48a044ce261275ee
César Marino <cesarmarino00@gmail.com>
2022-08-03 11:04:28 -0300

    ensure throw error with invalid params

--- src/usecase/product/create/create.product.unit.spec.ts
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -21,4 +21,21 @@ describe("Unit test a create product use case", () => {
 
         expect(output).toEqual({ id: expect.any(String), name: product.name, price: product.price });
     });
+
+    it("Should throw an error when name is missing", async () => {
+        const productRepository = mockRepository();
+        const usecase = new CreateProductUseCase(productRepository);
+
+        const input = { name: "", price: 50 };
+        await expect(usecase.execute(input)).rejects.toThrow("Name is required");
+    });
+
+    it("Should throw an error when price is missing", async () => {
+        const productRepository = mockRepository();
+        const usecase = new CreateProductUseCase(productRepository);
+
+        const input = { name: "Product 1", price: -1 };
+
+        await expect(usecase.execute(input)).rejects.toThrow("Price must be greater than zero");
+    });
 });
