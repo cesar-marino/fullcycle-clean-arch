96417271aa055af25d2d5f591caa7068fdcf1a33
César Marino <cesarmarino00@gmail.com>
2022-08-04 14:46:16 -0300

    ensure create a product to E2E test

---
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -0,0 +1,17 @@
+import { app, sequelize } from "../express";
+import request from "supertest";
+
+describe("E2E test for product", () => {
+    beforeEach(async () => await sequelize.sync({ force: true }));
+    afterAll(async () => await sequelize.close());
+    
+    it("Should create a product", async () => {
+        const response = await request(app)
+            .post("/product")
+            .send({ name: "Product 1", price: 50 });
+
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe("Product 1");
+        expect(response.body.price).toBe(50);
+    });
+});
