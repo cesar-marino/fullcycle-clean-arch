5098984a1c3d95dfb0ac9d1ec9bab3b30d974857
César Marino <cesarmarino00@gmail.com>
2022-08-04 14:47:50 -0300

    ensure not create a product to E2E test

--- src/infrastructure/api/__tests__/product.e2e.spec.ts
+++ src/infrastructure/api/__tests__/product.e2e.spec.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 describe("E2E test for product", () => {
     beforeEach(async () => await sequelize.sync({ force: true }));
     afterAll(async () => await sequelize.close());
-    
+
     it("Should create a product", async () => {
         const response = await request(app)
             .post("/product")
@@ -13,5 +13,13 @@ describe("E2E test for product", () => {
         expect(response.status).toBe(200);
         expect(response.body.name).toBe("Product 1");
         expect(response.body.price).toBe(50);
+    });
+
+    it("Should not create a product", async () => {
+        const response = await request(app)
+            .post("/product")
+            .send({ name: "Product 1", price: -10 });
+
+        expect(response.status).toBe(500);
     });
 });
